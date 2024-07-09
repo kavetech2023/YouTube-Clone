0c0b26d05b72fd8091576fd6530e3e63ccd11a19
kavetech2023 <kavetech@gmail.com>
2024-07-09 16:01:19 +0300

    create global css properties

--- youtube-clone/src/Components/Navbar/Navbar.jsx
+++ youtube-clone/src/Components/Navbar/Navbar.jsx
@@ -5,20 +5,20 @@ const Navbar = () => {
     return (
         <nav className="flex-div">
             <div className="nav-left flex-div">
-                <img className="menu-icon" src={menu_icon} alt=""/>
-                <img className="logo" src={logo} alt=""/>
+                <img className="menu-icon" src="" alt=""/>
+                <img className="logo" src="" alt=""/>
             </div>
 
             <div className="nav-middle flex-div">
                 <input type="text" placeholder="Search"/>
-                <img src={search_icon} alt=""/>
+                <img src="" alt=""/>
             </div>    
 
             <div className="nav-right flex-div">
                 <img src="" alt="" />
                 <img src="" alt="" />
                 <img src="" alt="" />
-                <img src="" alt="" />
+                <img src="" alt=""className="user-icon"/>
             </div> 
         </nav>
     )
