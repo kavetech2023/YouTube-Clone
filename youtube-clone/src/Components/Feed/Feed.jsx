03c33e903b23d595b74e574bfd25e6816325214d
kavetech2023 <kavetech@gmail.com>
2024-07-12 08:55:27 +0300

    style feed columns and rows

--- youtube-clone/src/Components/Feed/Feed.jsx
+++ youtube-clone/src/Components/Feed/Feed.jsx
@@ -3,13 +3,55 @@ import "./Feed.css"
 
 const Feed = () => {
     return (
-        <div>
+        <div className="feed">
             <div className="card">
                 <img src="" alt=""/>
                 <h2>Best channel to learn coding </h2>
                 <h3>Great stack</h3>
                 <p>15k views &bull; 2 days ago</p>
             </div>    
+            <div className="card">
+                <img src="" alt=""/>
+                <h2>Best channel to learn coding </h2>
+                <h3>Great stack</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
+            <div className="card">
+                <img src="" alt=""/>
+                <h2>Best channel to learn coding </h2>
+                <h3>Great stack</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
+            <div className="card">
+                <img src="" alt=""/>
+                <h2>Best channel to learn coding </h2>
+                <h3>Great stack</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
+            <div className="card">
+                <img src="" alt=""/>
+                <h2>Best channel to learn coding </h2>
+                <h3>Great stack</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
+            <div className="card">
+                <img src="" alt=""/>
+                <h2>Best channel to learn coding </h2>
+                <h3>Great stack</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
+            <div className="card">
+                <img src="" alt=""/>
+                <h2>Best channel to learn coding </h2>
+                <h3>Great stack</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
+            <div className="card">
+                <img src="" alt=""/>
+                <h2>Best channel to learn coding </h2>
+                <h3>Great stack</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
         </div>
     )
 }
