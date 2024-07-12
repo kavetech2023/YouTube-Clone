e07dae9bbf6c68fb4798133ab31d69bea6b411a8
kavetech2023 <kavetech@gmail.com>
2024-07-12 16:06:58 +0300

    Style home page

--- youtube-clone/src/pages/Home/Home.jsx
+++ youtube-clone/src/pages/Home/Home.jsx
@@ -2,29 +2,17 @@ import React from "react"
 import "./Home.css"
 import Sidebar from "../../Components/Navbar/Sidebar/Sidebar"
 import Feed from "../../Components/Feed/Feed"
+import { useState } from "react"
 
 const Home = ({sidebar}) => {
+
+    const [category, setCategory] = useState(0);
     return (
         <>
-        <Sidebar sidebar={sidebar}/>
+        <Sidebar sidebar={sidebar} category={category} setCategory={setCategory}/>
         <div className={`container ${sidebar?"":"large-container"}`}>
-            <Feed/>
-        </div>
-        <div className={`container ${sidebar?"":"large-container"}`}>
-            <Feed/>
-        </div><div className={`container ${sidebar?"":"large-container"}`}>
-            <Feed/>
-        </div><div className={`container ${sidebar?"":"large-container"}`}>
-            <Feed/>
-        </div><div className={`container ${sidebar?"":"large-container"}`}>
-            <Feed/>
-        </div><div className={`container ${sidebar?"":"large-container"}`}>
-            <Feed/>
-        </div><div className={`container ${sidebar?"":"large-container"}`}>
-            <Feed/>
-        </div><div className={`container ${sidebar?"":"large-container"}`}>
-            <Feed/>
-        </div>    
+            <Feed category={category}/>
+        </div>   
         </>
     )
     }   
