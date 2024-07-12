ea4853de40b90d1416f7fa89094f419a07d5293d
kavetech2023 <kavetech@gmail.com>
2024-07-12 15:03:02 +0300

    style recommended video components

---
+++ youtube-clone/src/Components/Recommended/Recommended.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import "./Recommended.css"
+
+const Recommended = () => {
+  return (
+    <div className='recommended'>
+// Recommended starts here
+        <div className="side-video-list">
+            <img src="" alt="" />
+            <div className="video-info">
+                <h4>Best channel to help you be a developer.</h4>
+                <p>Great stack</p>
+                <p>199K views</p>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src="" alt="" />
+            <div className="video-info">
+                <h4>Best channel to help you be a developer.</h4>
+                <p>Great stack</p>
+                <p>199K views</p>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src="" alt="" />
+            <div className="video-info">
+                <h4>Best channel to help you be a developer.</h4>
+                <p>Great stack</p>
+                <p>199K views</p>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src="" alt="" />
+            <div className="video-info">
+                <h4>Best channel to help you be a developer.</h4>
+                <p>Great stack</p>
+                <p>199K views</p>
+            </div>
+        </div>
+// Recommended ends here
+    </div>
+  )
+}
+
+export default Recommended
