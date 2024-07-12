1dcd3c5e95842774d75ebeb486035bd719a61909
kavetech2023 <kavetech@gmail.com>
2024-07-12 15:02:17 +0300

    style feed components

--- youtube-clone/src/Components/Feed/Feed.jsx
+++ youtube-clone/src/Components/Feed/Feed.jsx
@@ -1,15 +1,16 @@
 import React from "react"
 import "./Feed.css"
+import { Link } from "react-router-dom"
 
 const Feed = () => {
     return (
         <div className="feed">
-            <div className="card">
+            <Link to={`video/12/2`} className="card">
                 <img src="" alt=""/>
                 <h2>Best channel to learn coding </h2>
                 <h3>Great stack</h3>
                 <p>15k views &bull; 2 days ago</p>
-            </div>    
+            </Link>    
             <div className="card">
                 <img src="" alt=""/>
                 <h2>Best channel to learn coding </h2>
