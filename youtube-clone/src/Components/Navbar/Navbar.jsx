a5a7fbbc101057cf4d53764bb1eee28f5c101ec5
kavetech2023 <kavetech@gmail.com>
2024-07-15 23:34:39 +0300

    implement react-icons

--- youtube-clone/src/Components/Navbar/Navbar.jsx
+++ youtube-clone/src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,10 @@ import React from "react"
 import "./Navbar.css"
 import { useState } from "react"
 import { CgDetailsMore } from "react-icons/cg";
+import { FaYoutube } from "react-icons/fa6";
+import { FaUpload } from "react-icons/fa6";
+import { IoNotifications } from "react-icons/io5";
+import { FaSearch } from "react-icons/fa";
 import home from "../../assets/home.png"
 import Sidebar from "./Sidebar/Sidebar"
 import { Link } from "react-router-dom"
@@ -13,19 +17,21 @@ const Navbar = ({setSidebar}) => {
         <nav className="flex-div">
             <div className="nav-left flex-div">
                 <span className="menu-icon" onClick={()=>setSidebar(prev=>prev===false?true:false)}><CgDetailsMore size={30}/></span>
-                <Link to="/"><img className="logo" src={home} alt=""/></Link>
+                <Link to="/"><FaYoutube  size={40} color="red"/></Link>
             </div>
 
             <div className="nav-middle flex-div">
+                <div className="search-box flex-div">
                 <input type="text" placeholder="Search"/>
-                <img src="" alt=""/>
+                <span><FaSearch /></span>
+                </div>
+                
             </div>    
 
             <div className="nav-right flex-div">
-                <img src="" alt="" />
-                <img src="" alt="" />
-                <img src="" alt="" />
-                <img src="" alt=""className="user-icon"/>
+                <span><FaUpload size={25}/></span>
+                <span><IoNotifications size={25}/></span>
+                <img src={home} alt=""className="user-icon"/>
             </div> 
         </nav>
     )
