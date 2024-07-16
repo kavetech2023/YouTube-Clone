f2ef9b8ffd6fd8efef19d22a5b777abfbdabe939
kavetech2023 <kavetech@gmail.com>
2024-07-16 16:07:50 +0300

    add StrictMode component

--- youtube-clone/src/main.jsx
+++ youtube-clone/src/main.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
+import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import ContextProvider from './Context/Context.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ContextProvider>
+  <StrictMode>
     <BrowserRouter>
     <App />
     </BrowserRouter>
     
-  </ContextProvider>,
+  </StrictMode>,
 )
