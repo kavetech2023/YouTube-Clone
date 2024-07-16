172bb6c9d41a9a621e36d5bfe4d6918c631656e5
kavetech2023 <kavetech@gmail.com>
2024-07-16 16:06:49 +0300

    implement gemini api in feed

--- youtube-clone/src/Components/Feed/Feed.jsx
+++ youtube-clone/src/Components/Feed/Feed.jsx
@@ -1,11 +1,44 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState  } from "react"
 import "./Feed.css"
 import { Link } from "react-router-dom"
-import { API_KEY, value_converter } from "../../data"
-import { useState } from "react"
+import { API_KEY, value_converter } from "../../data.js"
 import moment from "moment"
+import run from "../../config/gemini.js"
+
+
+
+
+
 
 const Feed = ({category}) => {
+
+  //Modal start
+const [modal, setModal] = useState(true);
+const [moreInfoData, setMoreInfoData] = useState(null);
+
+const toggleModal = () => {
+  setModal(!modal);
+};
+//Modal ends here
+
+const [resultData, setResultData] = useState([]);
+
+
+const handleMoreInfo = async (channelTitle) => {
+    setModal(true);
+    try {
+      const processedData = await run("Explain to me " + channelTitle + "on youtube very briefly");
+      setMoreInfoData(processedData);
+      
+    } catch (error) {
+      console.error("Error processing data:", error);
+      // Handle errors gracefully (e.g., display an error message)
+    }
+  };
+
+
+
+
 
     const [data, setData] = useState([])
 
@@ -19,18 +52,40 @@ const Feed = ({category}) => {
     },[category])
     return (
         <div className="feed">
+<div className={`modal ${modal ? "show" : ""}`}>
+<div onClick={toggleModal} className="overlay"></div>
+<div className="modal-content">
+  <h2>Welcome to Youtube with Gemini Ai</h2>
+  <p>
+            {moreInfoData ? (
+              <span dangerouslySetInnerHTML={{ __html: moreInfoData }} /> // Safely render processed HTML
+            ) : (
+              "No additional information available yet"
+            )}
+          </p>
+  <button className="close-modal" onClick={toggleModal}>
+
+    CLOSE
+  </button>
+</div>
+</div>
+
             {data.map((item, index)=> {
         return(
-            <Link to={`video/${item.snippet.categoryId}/${item.id}`} className="card">
+            <div className="card" key={item.id}>
+            <Link to={`video/${item.snippet.categoryId}/${item.id}`} >
             <img src={item.snippet.thumbnails.medium.url} alt=""/>
-            <h2>{item.snippet.title}</h2>
-            <h3>{item.snippet.channelTitle}</h3>
-            <p> {value_converter(item.statistics.viewCount)}views &bull; {moment(item.snippet.publishedAt).fromNow()}</p>
-        </Link>
+            </Link>
+            <h2 onClick={toggleModal}>{item.snippet.title}</h2>
+            <h3 onClick={() => handleMoreInfo(item.snippet.channelTitle)}>
+            {item.snippet.channelTitle}
+          </h3>
+            <p>{value_converter(item.statistics.viewCount)}views &bull; {moment(item.snippet.publishedAt).fromNow()}</p>
+        </div>
 
         )
     })}
-                
+               
         </div>
     )
 }
