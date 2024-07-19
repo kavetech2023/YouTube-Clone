f4406f2fbb231a8d7e8eabe4f80d3a16f9d68588
kavetech2023 <kavetech@gmail.com>
2024-07-19 14:27:55 +0300

    comment code

--- youtube-clone/src/Components/PlayVideo/PlayVideo.jsx
+++ youtube-clone/src/Components/PlayVideo/PlayVideo.jsx
@@ -6,7 +6,7 @@ import moment from "moment";
 import { useParams } from "react-router-dom";
 
 const PlayVideo = () => {
-
+    // Fetching Video ID from URL
     const {videoId} = useParams();
 
     const [apiData, setApiData] = useState(null);
@@ -14,30 +14,33 @@ const PlayVideo = () => {
     const [commentData, setCommentData] = useState([]);
 
     const fetchVideoData = async () => {
-         // Fetching Videos Data
+         // Fetching Videos Data from Youtube API
          const videoDetails_url = `https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY}`;
          await fetch(videoDetails_url).then(res => res.json()).then(data => setApiData(data.items[0]));
 
-         //fetch comments data
+         //fetch comments data from youtube api
          const comment_url = `https://www.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&videoId=${videoId}&key=${API_KEY}`;
          await fetch(comment_url).then(res => res.json()).then(data => setCommentData(data.items));
     }
     
     const fetchChannelData = async () => {
-        // Fetching Channel Data
+        // Fetching Channel Data from Youtube API
         const channelDetails_url = `https://www.googleapis.com/youtube/v3/channels?part=snippet%2Cstatistics&id=${apiData.snippet.channelId}&key=${API_KEY}`;
         await fetch(channelDetails_url).then(res => res.json()).then(data => setChannelData(data.items[0]));
     }
+    // Fetching Data on Component Mount
     useEffect(() => {
         fetchVideoData()
     },[videoId])
 
+    // Fetching Data on Component Mount
     useEffect(() => {
         fetchChannelData()
     },[apiData])
 
 
     return (
+        // Display Video Player and Video Details
         <div className="play-video">
             <iframe className="new-iframe" src={`https://www.youtube.com/embed/${videoId}?autoplay=1`} controls autoPlay muted></iframe>
             <h3>{apiData?apiData.snippet.title:"Title here"}</h3>
